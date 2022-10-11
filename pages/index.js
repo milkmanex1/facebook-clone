74c3e8fbd3aed3768ee72341d888710a94431bca
Gabriel Kok <kokgabriel@gmail.com>
2022-10-11 22:21:51 +0800

    deployment version

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import Head from "next/head";
 import Header from "../components/Header";
-import { getSession } from "next-auth/react";
+import { getSession, useSession } from "next-auth/react";
 import Login from "../components/Login";
 import Sidebar from "../components/Sidebar";
 import Feed from "../components/Feed";
 import InputBox from "../components/InputBox";
 import Widgets from "../components/Widgets";
 import { useState } from "react";
+
 export default function Home({ session }) {
+  console.log(session);
   const backgrounds = [
     { id: 1, src: "/images/orion-nebula.jpg" },
     { id: 2, src: "/images/green.jpg" },
@@ -18,7 +20,7 @@ export default function Home({ session }) {
 
   const [bgIndex, setBgIndex] = useState(0);
 
-  if (!session) return <Login></Login>;
+  if (!session) return <Login />;
   return (
     <div
       className="h-screen overflow-hidden mainBg"
@@ -50,14 +52,6 @@ export default function Home({ session }) {
 export async function getServerSideProps(context) {
   //Get the user
   const session = await getSession(context);
-
-  //   const posts = await db.collection("posts").orderBy("timestamp", "desc").get();
-
-  //   const docs = posts.docs.map((post) => ({
-  //     id: post.id,
-  //     ...post.data(),
-  //     timestamp: null,
-  //   }));
 
   return {
     props: {
