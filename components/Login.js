f7d9a84c6269c24e78f5c0ef375b71f0da6b99db
Gabriel Kok <kokgabriel@gmail.com>
2022-10-11 21:15:57 +0800

    Added test account details to login page

--- components/Login.js
+++ components/Login.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import { signIn } from "next-auth/react";
+import { XCircleIcon } from "@heroicons/react/solid";
 // import '../styles/Login.css'
 
 const Login = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+  function openModal() {
+    setModalOpen(true);
+  }
+  function closeModal() {
+    setModalOpen(false);
+  }
+
   return (
     <div
       className="grid place-items-center h-screen bg-cover"
@@ -17,9 +26,33 @@ const Login = () => {
           ></Image> */}
         <div className="welcome">Spacebook</div>
 
-        <h1 className="loginBtn" onClick={signIn}>
+        <h1 className="loginBtn mb-5" onClick={signIn}>
           Login
         </h1>
+        {modalOpen ? (
+          <div className="text-white border-2 border-white p-2 rounded-2xl relative">
+            New here? Use this to login:
+            <XCircleIcon
+              className="h-6 w-6 absolute top-2 right-2 cursor-pointer"
+              onClick={closeModal}
+            />
+            <div className="grid grid-rows-2 ">
+              <div className="flex gap-4">
+                <div>Email:</div>testspacebook356@gmail.com
+              </div>
+              <div className="flex gap-4">
+                <div>Password:</div>password
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div
+            className="text-white/20 border-2 border-white/20 p-4 rounded-2xl cursor-pointer hover:text-white/90 hover:border-white/90 transition duration-500 ease-in-out"
+            onClick={openModal}
+          >
+            Welcome, stranger! Click Me to get Test Account details!
+          </div>
+        )}
       </div>
     </div>
   );
