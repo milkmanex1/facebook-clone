e90f9060612afae164f1f16c866952ac3c13a3fc
Gabriel Kok <kokgabriel@gmail.com>
2022-11-17 16:33:02 +0800

    //wordpress login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,6 +4,9 @@ import NextAuth from "next-auth/next";
 import FacebookProvider from "next-auth/providers/facebook";
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
+import RedditProvider from "next-auth/providers/reddit";
+import MediumProvider from "next-auth/providers/medium";
+import WordpressProvider from "next-auth/providers/wordpress";
 export const authOptions = {
   // Configure one or more authentication providers. Can be fb, google
   providers: [
@@ -18,6 +21,27 @@ export const authOptions = {
     GitHubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+    }),
+    // RedditProvider({
+    //   clientId: process.env.REDDIT_CLIENT_ID,
+    //   clientSecret: process.env.REDDIT_CLIENT_SECRET,
+    //   authorization: {
+    //     params: {
+    //       duration: "permanent",
+    //     },
+    //   },
+    // }),
+    // MediumProvider({
+    //     clientId: process.env.MEDIUM_CLIENT_ID,
+    //     clientSecret: process.env.MEDIUM_CLIENT_SECRET
+    //   })
+    // SlackProvider({
+    //     clientId: process.env.SLACK_CLIENT_ID,
+    //     clientSecret: process.env.SLACK_CLIENT_SECRET
+    //   })
+    WordpressProvider({
+      clientId: process.env.WORDPRESS_CLIENT_ID,
+      clientSecret: process.env.WORDPRESS_CLIENT_SECRET,
     }),
   ],
   //   callbacks: {
